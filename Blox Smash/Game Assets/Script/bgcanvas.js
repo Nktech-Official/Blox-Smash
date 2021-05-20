4d8639fd6e79daec7725bd4d9bbacf95c456a4c1
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2021-05-20 09:03:27 +0530

    initial commit

---
+++ Blox Smash/Game Assets/Script/bgcanvas.js
@@ -0,0 +1,19 @@
+const bgcanvas = document.getElementById("bgcanvas");
+const bgctx = bgcanvas.getContext("2d");
+bgcanvas.width = window.innerWidth;
+bgcanvas.height = window.innerHeight;
+const BgImageUrl = "../../Game Assets/img/game-background.jpg"
+let bgElement = document.querySelector("body")
+let BgImage = document.createElement("img");
+BgImage.src = BgImageUrl;
+BgImage.addEventListener("load", () => {
+    bgctx.drawImage(BgImage, 0, 0, bgcanvas.width, bgcanvas.height)
+    async function loading() {
+        await sleep(1000)
+        document.getElementById("loading").className = "fadeOut"
+        await sleep(2000)
+        document.getElementById("loading").style.display = "none"
+        GAMESNACKS.gameReady()
+    }
+    loading();
+})
